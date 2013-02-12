ad7c0fe77e415c6091f84c974c50e068f1a8408a
Chris Lamb <chris@lambynet.co.uk>
2013-02-12 00:09:01 +0000

    Setup the JS key too if we have it - it's optional.
    Signed-off-by: Chris Lamb <chris@lambynet.co.uk>

--- django_recurly/models.py
+++ django_recurly/models.py
@@ -3,3 +3,6 @@ import recurly
 from django.conf import settings
 
 recurly.API_KEY = settings.RECURLY_API_KEY
+
+if hasattr(settings, 'RECURLY_JS_SECRET_KEY'):
+    recurly.js.PRIVATE_KEY = settings.RECURLY_JS_PRIVATE_KEY
