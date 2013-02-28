af3f5944be82b6e6c9bfa59b889263e54c98da16
Chris Lamb <chris@lambynet.co.uk>
2013-02-28 23:21:27 +0000

    Correct reference to name.

--- django_recurly/models.py
+++ django_recurly/models.py
@@ -4,5 +4,5 @@ from django.conf import settings
 
 recurly.API_KEY = settings.RECURLY_API_KEY
 
-if hasattr(settings, 'RECURLY_JS_SECRET_KEY'):
+if hasattr(settings, 'RECURLY_JS_PRIVATE_KEY'):
     recurly.js.PRIVATE_KEY = settings.RECURLY_JS_PRIVATE_KEY
