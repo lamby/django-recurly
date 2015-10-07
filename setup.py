facb88a80cac3094171d5cc9f9d09fcd3469a9b9
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-07 09:49:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-recurly',
-    version=2,
-    license='BSD',
+
+    url="https://chris-lamb.co.uk/projects/django-recurly",
+    version='2',
+    description="Lightlight Recurly.com integration for Django",
+
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    license="BSD",
+
     packages=find_packages(),
-    url='https://chris-lamb.co.uk/projects/django-recurly',
-    author='Chris Lamb',
-    author_email='chris@chris-lamb.co.uk',
 )
